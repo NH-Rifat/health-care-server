a180c5667f3d41f0eb2b505e1a9addd9dcefa360
Hasan Rifat <rifatr729@gmail.com>
2025-01-15 18:23:08 +0600

    user and admin model has benn created with relation

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
-import express from "express";
+import { Server } from "http";
+import app from "./app";
 
-const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+async function main() {
+  const server: Server = await app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+
+  return server;
+}
+
+main();
