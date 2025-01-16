c0cc424934f83e280faaa06e98b073f5355d1dbb
Hasan Rifat <rifatr729@gmail.com>
2025-01-16 13:03:46 +0600

    admin user filtering with different property implimented successfully

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -3,6 +3,9 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getAllFromDB = async (params: any) => {
+  const { searchTerm, ...filteredData } = params;
+  console.log(params);
+  console.log(filteredData);
   const conditions = [];
   const searchAbleFields = ["name", "email"];
   if (params?.searchTerm) {
@@ -21,8 +24,33 @@ const getAllFromDB = async (params: any) => {
     );
   }
 
+  if (Object.keys(filteredData).length > 0) {
+    for (const key in filteredData) {
+      if (Object.hasOwnProperty.call(filteredData, key)) {
+        const value = filteredData[key];
+        if (value) {
+          conditions.push({
+            [key]: {
+              equals: value,
+            },
+          });
+        }
+      }
+    }
+  }
+
+  // if (Object.keys(filteredData).length > 0) {
+  //   conditions.push({
+  //     AND: Object.keys(filteredData).map((key) => ({
+  //       [key]: {
+  //         equals: filteredData[key],
+  //       },
+  //     })),
+  //   });
+  // }
+
   const whereConditions = conditions.length > 0 ? { AND: conditions } : {};
-  // console.log(whereConditions);
+  console.dir(whereConditions, { depth: "infinity" });
 
   const result = await prisma.admin.findMany({
     where: whereConditions,
