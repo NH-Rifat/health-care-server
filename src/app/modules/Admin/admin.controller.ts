7fb409f642ae28d7dc0145ba8289d6ad27ea6f00
Hasan Rifat <rifatr729@gmail.com>
2025-01-17 11:14:02 +0600

    controller req res handling from global catchAsync function act as a higher order function

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,11 +1,12 @@
+import { catchAsync } from "../../../shared/catchAsync";
 import { pickValidPropertyWithValue } from "../../../shared/pick";
 import { sendResponse } from "../../../shared/response";
 import { adminFilterableFields } from "./admin.constant";
 import { AdminService } from "./admin.service";
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 
-const getAllAdmin = async (req: Request, res: Response, next: NextFunction) => {
+const getAllAdmin: RequestHandler = catchAsync(async (req, res) => {
   const filtersQuery = pickValidPropertyWithValue(
     req.query,
     adminFilterableFields
@@ -16,53 +17,37 @@ const getAllAdmin = async (req: Request, res: Response, next: NextFunction) => {
     "sortBy",
     "sortOrder",
   ]);
-  try {
-    const result = await AdminService.getAllFromDB(filtersQuery, options);
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Admin users fetched successfully",
-      data: result?.data,
-      meta: result?.meta
-        ? {
-            total: result?.meta?.totalRecords,
-            page: result?.meta?.page,
-            limit: result?.meta?.limit,
-          }
-        : undefined,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  const result = await AdminService.getAllFromDB(filtersQuery, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Admin users fetched successfully",
+    data: result?.data,
+    meta: result?.meta
+      ? {
+          total: result?.meta?.totalRecords,
+          page: result?.meta?.page,
+          limit: result?.meta?.limit,
+        }
+      : undefined,
+  });
+});
 
-const getAdminById = async (req: Request, res: Response) => {
+const getAdminById = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
-  try {
-    const result = await AdminService.getAdminByIdFromDB(id);
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Admin user fetched successfully",
-      data: result,
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err instanceof Error ? err.name : "Internal server error",
-      data: null,
-    });
-  }
-};
+  const result = await AdminService.getAdminByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Admin user fetched successfully",
+    data: result,
+  });
+});
 
-const updateAdminById = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const { id } = req.params;
-  const data = req.body;
-  try {
+const updateAdminById = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const data = req.body;
     const result = await AdminService.updateAdminByIdFromDB(id, data);
     sendResponse(res, {
       statusCode: httpStatus.OK,
@@ -70,18 +55,12 @@ const updateAdminById = async (
       message: "Admin user updated successfully",
       data: result,
     });
-  } catch (err) {
-    next(err);
   }
-};
+);
 
-const deleteAdminById = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const { id } = req.params;
-  try {
+const deleteAdminById = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
     const result = await AdminService.softDeleteAdminByIdFromDB(id);
     sendResponse(res, {
       statusCode: httpStatus.OK,
@@ -89,15 +68,8 @@ const deleteAdminById = async (
       message: "Admin user deleted successfully",
       data: result,
     });
-  } catch (err) {
-    // res.status(500).json({
-    //   success: false,
-    //   message: err instanceof Error ? err.name : "Internal server error",
-    //   data: null,
-    // });
-    next(err);
   }
-};
+);
 
 export const adminController = {
   getAllAdmin,
