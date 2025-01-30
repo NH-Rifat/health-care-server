8bec6f5301e1ba3c81ebd197fceb58d4827e5534
Hasan Rifat <rifatr729@gmail.com>
2025-01-30 18:03:56 +0600

    handle prisma error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 
@@ -7,10 +8,25 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-    success: false,
-    message: err.message,
-    error: err,
+  let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = err.message;
+  let error = err;
+
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    message = "Validation Error";
+    error = err.message;
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      message = "Unique Constraint Violation";
+      error = err.message;
+    }
+  }
+
+  res.status(statusCode).json({
+    success,
+    message,
+    error,
   });
 };
 
